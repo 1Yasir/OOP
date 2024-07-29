ffc3c87aac772e327bc38f1bfba4e512c12bca33
Yasir Mehmood <129255451+1Yasir@users.noreply.github.com>
2024-07-29 22:04:04 +0500

    add two function addtodo & render

--- js/app.js
+++ js/app.js
@@ -6,18 +6,44 @@ const lists = [];
 
 class Todo {
 
-    constructor(task, completed = false) {
+    constructor(task, completed = false, createTime, updateTime) {
         this.task = task;
         this.completed = completed;
-        this.createTime = new Date.now().toString();
-        this.updateTime = new Date.now().toString();
+        // this.createTime = new Date.now().toString();
+        // this.updateTime = new Date.now().toString();
     }
 
-    static addToDo(){
+    static addToDo() {
 
         const inputText = inputElement.value.trim();
 
-        if(!inputElement) return alert("add task ......");
+        if (!inputText) return alert("add task ......");
+
+        const newTask = new Todo(inputText);
+        lists.push(newTask);
+        inputElement.value = ""
+        Todo.render();
 
     }
+
+
+    static render() {
+        ulElement.innerHTML = "";
+
+        lists.forEach((todo, index) => {
+            const li = document.createElement("li");
+            li.classList = todo.completed ? "complete" : "";
+            li.innerHTML = `
+            <span>${todo.task}</span>
+                <button class="edit" onclick="Todo.editTodoPrompt(${index})">Edit</button>
+                <button onclick="Todo.removeTodo(${index})">Delete</button>
+                <button class="complete" onclick="Todo.toggleComplete(${index})">Complete</button>
+            `
+
+            ulElement.prepend(li);
+        })
+    }
 }
+
+
+btnElement.addEventListener("click", Todo.addToDo);
